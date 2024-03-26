3fa59dc92621867aa76580e305ca3c7b62e236b9
LDZ-ps7422 <ldz000611@163.com>
2024-03-26 08:56:44 +0800

    feat: using bufwriter

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,32 @@
 use std::fs::File;
-use std::io::prelude::*;
+use std::io::{prelude::*, BufWriter};
 use std::thread;
 
 fn main() -> std::io::Result<()> {
-    let file_num: u32 = 5;
-    let file_size: u32 = 100000;
+    let file_num: u32 = 20;
+    let file_size: u32 = 100000000;
 
     let mut handles = vec![];
 
     for idx in 1..=file_num {
-
         let handle = thread::spawn(move || {
-
-            let mut file = File::create(format!("./target/data{}.csv", idx))
+            let file = File::create(format!("./target/data{}.csv", idx))
                 .expect("ERROR: fail to create a file!");
 
-            for line_i in 1..file_size {
-                file.write_all(format!("{:0>6},AAA,100\n", line_i).as_bytes())
-                    .expect("ERROR: fail to write data!");
+            let mut writer = BufWriter::new(file);
+
+            for line_i in 1..=file_size {
+                if line_i == file_size {
+                    writeln!(writer, "{:0>8},AAA,100", line_i)
+                        .expect("ERROR: fail to write data!");
+                } else {
+                    writeln!(writer, "{:0>8},AAA,100", line_i)
+                        .expect("ERROR: fail to write data!");
+                }
             }
-            file.write_all(format!("{:0>6},AAA,100", file_size).as_bytes())
-                .expect("ERROR: fail to write data!");
-    
-            
+
+            writer.flush().expect("ERROR: fail to flush buffer!");
+
             println!("Data has been written to data{}.csv 🚀", idx);
         });
 
@@ -34,5 +38,4 @@ fn main() -> std::io::Result<()> {
     }
 
     Ok(())
-
 }
